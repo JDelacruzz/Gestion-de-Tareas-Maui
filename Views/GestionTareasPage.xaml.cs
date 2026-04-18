f6b70da081b79d5a616517f9595f08c5045d5e32
JDelacruzz <delacruzjuan07@hotmail.com>
2026-04-17 21:19:39 -0500

    Se cambia la forma de cambio de estado con el boton

--- Views/GestionTareasPage.xaml.cs
+++ Views/GestionTareasPage.xaml.cs
@@ -1,17 +1,13 @@
-// Views/GestionTareasPage.xaml.cs
 using Gestion_de_Tareas.ViewModels;
 
 namespace Gestion_de_Tareas.Views
 {
     public partial class GestionTareasPage : ContentPage
     {
-        private readonly TareasViewModel _vm;
-
         public GestionTareasPage(TareasViewModel viewModel)
         {
             InitializeComponent();
-            _vm = viewModel;
-            BindingContext = _vm; 
+            BindingContext = viewModel;
         }
     }
 }
